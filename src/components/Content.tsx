3e77c771f953a15330fe3e4d7d1f239fb19399b6
thayron Matos <thayron.f3@gmail.com>
2022-07-01 04:30:31 -0300

    dividindo a responsabilidade em componentes

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface IPropsContent {
+  movies: MovieProps[];
+}
+
+export function Content({ movies }: IPropsContent) {
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map((movie: MovieProps) => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
